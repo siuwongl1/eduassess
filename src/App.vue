caead039dbe9de8c4f22b9def9ab103cc2afc0a8
黎兆旺 <siuwongli1994@gmail.com>
2017-03-23 20:54:58 +0800

    添加登录页面的样式，加入vue路由依赖包

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
   <div id="app">
       <el-card class="box-card">
-          <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tabs v-model="activeName" style="text-align:center" @tab-click="handleClick">
               <el-tab-pane label="登录" name="first">
                   <el-form>
                   <el-input v-model="input" class="item" placeholder="用户名或邮箱"></el-input>
                   <el-input v-model="input" type="password" class="item" placeholder="密码"></el-input>
-                  <div class="item-radio">
-                    <el-radio class="radio" v-model="radio" label="student">学生</el-radio>
-                    <el-radio class="radio" v-model="radio" label="teacher">教师</el-radio>
-                    <el-radio class="radio" v-model="radio" label="instructor">教学管理员</el-radio>
-                    <el-radio class="radio" v-model="radio" label="admin">管理员</el-radio>
-                  </div>
+                  <el-radio-group v-model="type" size="small" class="item-radio">
+                      <el-radio :label="1">学生</el-radio>
+                      <el-radio :label="2">教师</el-radio>
+                      <el-radio :label="3">教学管理员</el-radio>
+                      <el-radio :label="4">管理员</el-radio>
+                    </el-radio-group>
                   <el-button type="primary" class="item-btn-login">登录</el-button>
                                       <p class="item-p"><a class="item-p-link" href="#">无法登录？</a></p>
-
                   </el-form>
               </el-tab-pane>
               <el-tab-pane label="注册" name="second">
-
+                   <el-form>
+                         <el-input v-model="input" class="item" placeholder="用户名"></el-input>
+                         <el-input v-model="input" class="item" placeholder="邮箱"></el-input>
+                         <el-input v-model="input" type="password" class="item" placeholder="密码"></el-input>
+                         <el-input v-model="input" type="password" class="item" placeholder="确认密码"></el-input>
+                         <el-radio-group v-model="type" size="small" class="item-radio">
+                             <el-radio :label="1">学生</el-radio>
+                             <el-radio :label="2">教师</el-radio>
+                             <el-radio :label="3">教学管理员</el-radio>
+                             <el-radio :label="4">管理员</el-radio>
+                           </el-radio-group>
+                         <el-button type="primary" class="item-btn-login">注册</el-button>
+                    </el-form>
               </el-tab-pane>
           </el-tabs>
       </el-card>
-    <p class="footer">
-        ©️2017 浙江工业大学
-    </p>
   </div>
 </template>
 
@@ -32,7 +40,7 @@
 export default {
   data () {
     return {
-      radio: 'student',activeName: 'first'
+      radio: 'student',activeName: 'first',type:1
     }
   },
   methods: {
@@ -79,13 +87,7 @@ export default {
   }
   .box-card {
     padding:10px;
-    text-align:center;
     margin:0 auto;
     width: 480px;
-  }
-  .footer{
-    text-align:center;
-    font-size:12px;
-    color: #aebdc9;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 
 Vue.use(ElementUI)
-
+Vue.use(VueRouter)
 new Vue({
   el: '#app',
   render: h => h(App)
